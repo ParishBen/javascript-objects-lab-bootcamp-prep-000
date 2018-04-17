999f3e63618c0a6c435036d0cef193d6a7be866c
parishben13@gmail.com <parishben13@gmail.com>
2018-04-17 04:36:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var recipes= {prop: 1}
 function updateObjectWithKeyAndValue(object, prop2, value){
-  Object.assign({}, recipes, { [prop2]: 2 })
+  Object.assign({}, recipes, { [prop]:1, [prop2]: 2 })
   return recipes
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
